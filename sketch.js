ad8169f4ef0f5d165ea2b0d652e26dcdab145f68
akpanther <45427189+akpanther@users.noreply.github.com>
2020-12-27 16:22:01 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function setup() {
 function draw() {
   if (gameState === "start") {
     background("black");
-   
+   text("up arrow to start")
   if (keyCode === UP_ARROW) {
       gameState = "play"
     }
